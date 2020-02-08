0026240c0b0ec0d629f9398a4a012d406c8da79f
kouui <lelouchkako@gmail.com>
2020-02-08 16:13:03 +0900

    write index.html --> render index.html

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from flask import Flask, render_template, request
 from make_html import get_multi_table_string
 app = Flask(__name__)
 
+global_var = {
+    "table_string" : "",
+}
+
 
 @app.route('/generate')
 def generate_index_html():
@@ -16,34 +20,38 @@ def generate_index_html():
 
     fname = {
     "template" : {"table" : "./contents/table.template",
-                  "index" : "./contents/index.template",
-                  "style" : "./contents/style.template"},
-    "output" : "./templates/index.html",
+    #              "index" : "./contents/index.template",
+    #              "style" : "./contents/style.template"
+                 },
+    #"output" : "./templates/index.html",
     }
 
     #word_list = ["えび","ホタテ","小松菜"]
 
     multi_table_string = get_multi_table_string(word_list, fname_dict=fname, n_recipe=3, n_combination=2)
 
-    with open(fname["template"]["index"], 'r') as f:
-        index_string = f.read()
-    with open(fname["template"]["style"], 'r') as f:
-        style_string = f.read()
+    #-- GAE's file system is read only
+    #with open(fname["template"]["index"], 'r') as f:
+    #    index_string = f.read()
+    #with open(fname["template"]["style"], 'r') as f:
+    #    style_string = f.read()
 
-    index_string = index_string.replace("{style}", style_string)
-    index_string = index_string.replace("{table}", multi_table_string)
+    #index_string = index_string.replace("{style}", style_string)
+    #index_string = index_string.replace("{table}", multi_table_string)
 
-    with open(fname["output"], 'w') as f:
-        f.write(index_string)
+    #with open(fname["output"], 'w') as f:
+    #    f.write(index_string)
 
-    return "link : <a href='http://127.0.0.1:8080/'>http://127.0.0.1:8080/</a>"
+    global_var["table_string"] = multi_table_string
+
+    return "link : <a href='http://https://recipe-recommendation-267607.appspot.com/'>http://https://recipe-recommendation-267607.appspot.com/</a>"
 
 @app.route('/')
 def index_html():
     r"""
     """
 
-    return render_template('index.html')
+    return render_template('index.html', table_string=global_var["table_string"])
 
 
 if __name__ == "__main__":
